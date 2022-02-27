c14abce9886355d5bd7ff24b9cf56f125a10a6f8
Nick Harris <nharris614@gmail.com>
2022-02-27 16:22:39 -0600

    Change skip to false

--- _data/sites/nick-harris-dot-me.js
+++ _data/sites/nick-harris-dot-me.js
@@ -1,9 +1,7 @@
 module.exports = {
   name: "nickharris.me", // optional, falls back to object key
   description: "Nick’s Personal web site",
-  // skip if localhost
-  // skip if this is a new fork of the speedlify (not Zach’s)
-  skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+  skip: false,
   options: {
     frequency: 60 * 23, // 23 hours
     // Use "run" if the sites don’t share assets on the same origin
